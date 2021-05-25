e1eda47ff7fa6b637af4b74d329d7155640811b3
Alireza Noorani <noorani.alireza@gmail.com>
2021-05-25 20:49:22 +0430

    Add Singleton pattern.

---
+++ DesignPatternSamples/Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singleton.Instance.Operation();
+            Singleton.Instance.Operation();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternSamples/Singleton/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> _instance = new(() => new Singleton(), true);
+        public static Singleton Instance => _instance.Value;
+
+        private Guid ID = Guid.NewGuid();        
+
+        private Singleton() { }
+
+        public void Operation()
+        {
+            Console.WriteLine($"Singleton({ID}) ===> Operation");
+        }
+    }
+}
